90b73040f5fbe6fa0e8867c6d1dbce0797f0e6b1
Jan du Plessis <drjanduplessis@icloud.com>
2025-01-14 11:03:42 +0000

    Automated commit via Python script - 2025-01-14 11:03

--- src/practice_pilot/streamlit_app.py
+++ src/practice_pilot/streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+
+page_element="""
+<style>
+[data-testid="stAppViewContainer"]{
+  background-image: url("https://cdn.wallpapersafari.com/88/75/cLUQqJ.jpg");
+  background-size: cover;
+}
+</style>
+"""
+
+st.markdown(page_element, unsafe_allow_html=True)
